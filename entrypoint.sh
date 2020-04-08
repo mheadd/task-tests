a5b311a642012de3cb3acbc0e9c2c0a0f5231251
Mark Headd <mheadd@gmail.com>
2020-04-08 14:53:39 -0400

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,21 +4,21 @@ APP_NAME=$1
 TASK=$2
 TASK_NAME=$3
 
-if [[ ! -r "$APP_NAME" ]]; then
+if [[ -z ${APP_NAME} ]]; then
   echo "You must specify an app name"
   exit 1
 fi
 
-if [[ ! -r "$TASK" ]]; then
+if [[ -z ${TASK} ]]; then
   echo "You must specify a task to run"
   exit 1
 fi
 
-if [[ ! -r "$TASK_NAME" ]]; then
+if [[ -z ${TASK_NAME} ]]; then
   echo "You must specify a name for your task"
   exit 1
 fi
 
 # cf run-task $1 "$2" --name $3
 
-cf --help
+cf --version
