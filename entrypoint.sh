5d291677c1438474177fbdc7e3e528e2776917e5
Mark Headd <mheadd@gmail.com>
2020-04-08 14:26:46 -0400

    Fix script

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,17 @@ APP_NAME=$1
 TASK=$2
 TASK_NAME=$3
 
-if [[ ! -r "$1" ]]; then
+if [[ ! -r "$APP_NAME" ]]; then
   echo "You must specify an app name"
   exit 1
 fi
 
-if [[ ! -r "$2" ]]; then
+if [[ ! -r "$TASK" ]]; then
   echo "You must specify a task to run"
   exit 1
 fi
 
-if [[ ! -r "$3" ]]; then
+if [[ ! -r "$TASK_NAME" ]]; then
   echo "You must specify a name for your task"
   exit 1
 fi
